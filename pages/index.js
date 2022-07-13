ad15c0d68935eaedd162bd2251ec4423cc37a205
Candy Fair <candice.bastien@gmail.com>
2022-07-13 18:45:58 +0200

    Test to display posts images

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import { getPosts } from "../lib/functions";
 import Link from "next/link";
+import Image from "next/image";
 
-const Index = (props) => (
-  <ul>
-    {props.posts.map((post) => (
-      <li key={post.id}>
-        <Link href={`/posts/${post.slug}`}>
-          <a>{post.title}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
+import styles from '../styles/Home.module.css'
+
+const Home = (props) => (
+  <div className={styles.container}>
+    <header className={styles.header}>
+      <h1 className={styles.title}>Blog</h1>
+    </header>
+    <main>
+      <ul>
+        {props.posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+            <img src={post.feature_image} alt={post.title} width={400} height={250} />
+            <p>{post.feature_image}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
 );
 
-export default Index;
+export default Home;
 
 // Generate this page at build time
 export async function getStaticProps(context) {
